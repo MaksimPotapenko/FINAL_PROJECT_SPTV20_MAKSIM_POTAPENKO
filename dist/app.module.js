b05f1d3ac83993ba0ea29e43748decdf0300a8c4
Max <maks.potapenko@gmail.com>
2024-02-02 05:32:51 +0200

    users added. test database connected. start making auth module

--- dist/app.module.js
+++ dist/app.module.js
@@ -8,16 +8,34 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
-const app_controller_1 = require("./app.controller");
-const app_service_1 = require("./app.service");
+const users_module_1 = require("./users/users.module");
+const sequelize_1 = require("@nestjs/sequelize");
+const config_1 = require("@nestjs/config");
+const sequelizeConfig_service_1 = require("./config/sequelizeConfig.service");
+const configuration_1 = require("./config/configuration");
+const auth_module_1 = require("./auth/auth.module");
+const boiler_parts_module_1 = require("./boiler-parts/boiler-parts.module");
+const shopping_cart_module_1 = require("./shopping-cart/shopping-cart.module");
+const payment_module_1 = require("./payment/payment.module");
 let AppModule = class AppModule {
 };
-exports.AppModule = AppModule;
-exports.AppModule = AppModule = __decorate([
+AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
-        controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
+        imports: [
+            sequelize_1.SequelizeModule.forRootAsync({
+                imports: [config_1.ConfigModule],
+                useClass: sequelizeConfig_service_1.SequelizeConfigService,
+            }),
+            config_1.ConfigModule.forRoot({
+                load: [configuration_1.databaseConfig],
+            }),
+            users_module_1.UsersModule,
+            auth_module_1.AuthModule,
+            boiler_parts_module_1.BoilerPartsModule,
+            shopping_cart_module_1.ShoppingCartModule,
+            payment_module_1.PaymentModule,
+        ],
     })
 ], AppModule);
+exports.AppModule = AppModule;
 //# sourceMappingURL=app.module.js.map
